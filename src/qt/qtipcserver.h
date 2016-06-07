2c9beab63f7fc6ba2faeff00faa9a6d38f82b728
404 <404coin@scryptmail.com>
2016-06-07 13:00:47 +0200

    404

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define 404coin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "404coinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
